3e0fa93b6b5c4a87b0293f06209c2f7ed1d46726
gersonEspinoza722 <39819821+gersonEspinoza722@users.noreply.github.com>
2019-11-05 14:23:21 -0600

    GUI implemented, selection text logic start

---
+++ .idea/src/Word.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+
+public class Word {
+    private String content;
+    private Color color;
+
+    public Word(String content, Color color) {
+        this.content = content;
+        this.color = color;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+}
