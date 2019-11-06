cde656184220619b94bbaa83707461647506e3bd
gersonEspinoza722 <39819821+gersonEspinoza722@users.noreply.github.com>
2019-11-06 11:19:11 -0600

    color change done

--- src/editor.java
+++ src/editor.java
@@ -7,45 +7,48 @@ import javax.swing.plaf.metal.*;
 import javax.swing.text.*;
 
 class editor extends JFrame implements ActionListener {
-    // Text component 
-    JTextPane t;
-    ArrayList<Integer> changed;
+    // Text component
+    private JTextPane t;
+    private ArrayList<Integer> changed;
+    private ArrayList<ArrayList<WordObject>> words;
+    boolean tokenized=false;
 
-    // Frame 
+    // Frame
     JFrame f;
 
-    // Constructor 
+    // Constructor
     editor()
     {
-        // Create a frame 
+        words = new ArrayList<ArrayList<WordObject>>();
+        // Create a frame
         f = new JFrame("editor");
 
         try {
-            // Set metl look and feel 
+            // Set metl look and feel
             UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
 
-            // Set theme to ocean 
+            // Set theme to ocean
             MetalLookAndFeel.setCurrentTheme(new OceanTheme());
         }
         catch (Exception e) {
         }
 
-        // Text component 
+        // Text component
         t = new JTextPane();
 
-        // Create a menubar 
+        // Create a menubar
         JMenuBar mb = new JMenuBar();
 
-        // Create amenu for menu 
+        // Create amenu for menu
         JMenu m1 = new JMenu("File");
 
-        // Create menu items 
+        // Create menu items
         JMenuItem mi1 = new JMenuItem("New");
         JMenuItem mi2 = new JMenuItem("Open");
         JMenuItem mi3 = new JMenuItem("Save");
         JMenuItem mi9 = new JMenuItem("Print");
 
-        // Add action listener 
+        // Add action listener
         mi1.addActionListener(this);
         mi2.addActionListener(this);
         mi3.addActionListener(this);
@@ -56,15 +59,15 @@ class editor extends JFrame implements ActionListener {
         m1.add(mi3);
         m1.add(mi9);
 
-        // Create amenu for menu 
+        // Create amenu for menu
         JMenu m2 = new JMenu("Edit");
 
-        // Create menu items 
+        // Create menu items
         JMenuItem mi4 = new JMenuItem("cut");
         JMenuItem mi5 = new JMenuItem("copy");
         JMenuItem mi6 = new JMenuItem("paste");
         JMenuItem mi7 = new JMenuItem("color");
-        // Add action listener 
+        // Add action listener
         mi4.addActionListener(this);
         mi5.addActionListener(this);
         mi6.addActionListener(this);
@@ -88,7 +91,7 @@ class editor extends JFrame implements ActionListener {
         f.show();
     }
 
-    // If a button is pressed 
+    // If a button is pressed
     public void actionPerformed(ActionEvent e)
     {
         String s = e.getActionCommand();
@@ -118,48 +121,61 @@ class editor extends JFrame implements ActionListener {
             for (int i = 0; i < renglonesCompletos.length; i++){
                 renglonesTokenizados.add(renglonesCompletos[i].split(" "));
             }
-            ArrayList<ArrayList<WordObject>> words = new ArrayList<ArrayList<WordObject>>();
+            //ArrayList<ArrayList<WordObject>> words = new ArrayList<ArrayList<WordObject>>();
 
-            for(int j =0; j<renglonesTokenizados.size();j++){
-                ArrayList<WordObject> row = new ArrayList<>();
-                for(int x = 0; x<renglonesTokenizados.get(j).length;x++){
+            if(words.size()==0){
+                for(int j =0; j<renglonesTokenizados.size();j++){
+                    ArrayList<WordObject> row = new ArrayList<>();
+                    for(int x = 0; x<renglonesTokenizados.get(j).length;x++){
 
-                    System.out.println(renglonesTokenizados.get(j)[x]);
-                    WordObject wordToken = new WordObject(renglonesTokenizados.get(j)[x],Color.BLACK);
-                    //System.out.println(wordToken.getContent());
-                    row.add(wordToken);
-                    //System.out.println(row.size());
+                        //System.out.println(renglonesTokenizados.get(j)[x]);
+                        WordObject wordToken = new WordObject(renglonesTokenizados.get(j)[x],t.getSelectedTextColor());
+                        //System.out.println(wordToken.getContent());
+                        row.add(wordToken);
+                        //System.out.println(row.size());
+                    }
+                    words.add(row);
+                    //System.out.println(words.size());
                 }
-                words.add(row);
-                //System.out.println(words.size());
+                tokenized=true;
             }
+
 
             int wordCounter=0;
 
             t.setText("");
-            System.out.println("selección: "+selectionPos);
+            //System.out.println("selección: "+selectionPos);
             for(int r=0;r<words.size();r++){
                 for(int w=0;w<words.get(r).size();w++){
-                    System.out.println("contador: "+wordCounter);
+                    //System.out.println("contador: "+wordCounter);
                     if(wordCounter==selectionPos){
+                        System.out.println("la encontro");
                         words.get(r).get(w).setColor(Color.red);
                     }
                     appendToPane(t,words.get(r).get(w).getContent(),words.get(r).get(w).getColor());
-                    appendToPane(t," ",Color.BLACK);
+                    if(w!=words.get(r).size()-1){
+                        appendToPane(t," ",Color.BLACK);
+                    }
+
                     wordCounter+=words.get(r).get(w).getContent().length();
                     if(words.get(r).size()>1 && w<words.get(r).size()-1){
                         wordCounter+=1;
                     }
 
                 }
+                if(tokenized==true){
+                    wordCounter+=1;
+                }
+
                 appendToPane(t,"\n",Color.BLACK);
             }
 
 
 
 
-            System.out.println("Renglones: " +renglonesTokenizados.size());
-            System.out.println("Primer renglón tiene: "+renglonesTokenizados.get(0).length);
+
+            //System.out.println("Renglones: " +renglonesTokenizados.size());
+            // System.out.println("Primer renglón tiene: "+renglonesTokenizados.get(0).length);
 
 
             //appendToPane(t,w1.getContent(),w1.getColor());
@@ -167,25 +183,25 @@ class editor extends JFrame implements ActionListener {
             //appendToPane(t,w3.getContent(),w3.getColor());
         }
         else if (s.equals("Save")) {
-            // Create an object of JFileChooser class 
+            // Create an object of JFileChooser class
             JFileChooser j = new JFileChooser("f:");
 
-            // Invoke the showsSaveDialog function to show the save dialog 
+            // Invoke the showsSaveDialog function to show the save dialog
             int r = j.showSaveDialog(null);
 
             if (r == JFileChooser.APPROVE_OPTION) {
 
-                // Set the label to the path of the selected directory 
+                // Set the label to the path of the selected directory
                 File fi = new File(j.getSelectedFile().getAbsolutePath());
 
                 try {
-                    // Create a file writer 
+                    // Create a file writer
                     FileWriter wr = new FileWriter(fi, false);
 
-                    // Create buffered writer to write 
+                    // Create buffered writer to write
                     BufferedWriter w = new BufferedWriter(wr);
 
-                    // Write 
+                    // Write
                     w.write(t.getText());
 
                     w.flush();
@@ -195,13 +211,13 @@ class editor extends JFrame implements ActionListener {
                     JOptionPane.showMessageDialog(f, evt.getMessage());
                 }
             }
-            // If the user cancelled the operation 
+            // If the user cancelled the operation
             else
                 JOptionPane.showMessageDialog(f, "the user cancelled the operation");
         }
         else if (s.equals("Print")) {
             try {
-                // print the file 
+                // print the file
                 t.print();
             }
             catch (Exception evt) {
@@ -209,43 +225,43 @@ class editor extends JFrame implements ActionListener {
             }
         }
         else if (s.equals("Open")) {
-            // Create an object of JFileChooser class 
+            // Create an object of JFileChooser class
             JFileChooser j = new JFileChooser("f:");
 
-            // Invoke the showsOpenDialog function to show the save dialog 
+            // Invoke the showsOpenDialog function to show the save dialog
             int r = j.showOpenDialog(null);
 
-            // If the user selects a file 
+            // If the user selects a file
             if (r == JFileChooser.APPROVE_OPTION) {
-                // Set the label to the path of the selected directory 
+                // Set the label to the path of the selected directory
                 File fi = new File(j.getSelectedFile().getAbsolutePath());
 
                 try {
-                    // String 
+                    // String
                     String s1 = "", sl = "";
 
-                    // File reader 
+                    // File reader
                     FileReader fr = new FileReader(fi);
 
-                    // Buffered reader 
+                    // Buffered reader
                     BufferedReader br = new BufferedReader(fr);
 
-                    // Initilize sl 
+                    // Initilize sl
                     sl = br.readLine();
 
-                    // Take the input from the file 
+                    // Take the input from the file
                     while ((s1 = br.readLine()) != null) {
                         sl = sl + "\n" + s1;
                     }
 
-                    // Set the text 
+                    // Set the text
                     t.setText(sl);
                 }
                 catch (Exception evt) {
                     JOptionPane.showMessageDialog(f, evt.getMessage());
                 }
             }
-            // If the user cancelled the operation 
+            // If the user cancelled the operation
             else
                 JOptionPane.showMessageDialog(f, "the user cancelled the operation");
         }
@@ -271,9 +287,9 @@ class editor extends JFrame implements ActionListener {
         tp.replaceSelection(msg);
     }
 
-    // Main class 
+    // Main class
     public static void main(String args[])
     {
         editor e = new editor();
     }
-} 
+}
